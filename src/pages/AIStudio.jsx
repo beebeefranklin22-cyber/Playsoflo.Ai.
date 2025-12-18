73ea8a032ca4394314f52185b6b8596d99b116a2
donsweep381 <donsweep381@gmail.com>
2025-12-18 05:07:36 +0000

    File changes

--- src/pages/AIStudio.jsx
+++ src/pages/AIStudio.jsx
@@ -676,7 +676,7 @@ Be production-ready and technically accurate.`;
               </div>
 
               {/* Demo Output */}
-              {generationOutput && (
+              {demoOutput && (
                <motion.div
                  initial={{ opacity: 0, y: 20 }}
                  animate={{ opacity: 1, y: 0 }}
